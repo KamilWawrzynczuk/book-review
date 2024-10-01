56acb2178076b1309993cc98aba8e57d3d58f36e
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2024-10-01 14:01:20 +0200

    Add form to search books in index view

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,11 @@
 @section('content')
     <h1 class="mb-10 text-2xl">Books</h1>
 
-    <form></form>
+    <form method="GET" actions="{{ route('books.index')}}" class="mb-4 flex items-center gap-4">
+        <input class="input h-10" type="text" name="title" placeholder="Search by title.."  value="{{ request('title') }}"/>
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
+    </form>
 
     <ul>
         @forelse ($books as $book)
